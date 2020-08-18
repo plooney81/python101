f70fd01571353daef6d0939c2e267ac68de3bfe9
Pete Looney <petelooney81@gmail.com>
2020-08-18 14:48:15 -0500

    Added hello2.py Assignment 2) for python101

---
+++ hello2.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter their name
+print('Please enter your name below')
+
+# save the input to a variable name user_name and then change that to all upper case
+user_name = input('> ').upper()
+
+#print the upper case version of the user name
+print(f'Here is your name all in upper case: {user_name}. How cool is that ?')
