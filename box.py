67171de71a97d040e63a5334ec963c49d6165e1f
Pete Looney <petelooney81@gmail.com>
2020-08-19 11:02:12 -0500

    added box.py

---
+++ box.py
@@ -0,0 +1,33 @@
+# Function that checks to see if the input is an integer, if it isn't an integer, then the while
+# loop will not 
+def is_it_a_integer():
+    while True:
+        try:
+            return int(input('> '))
+            break
+        except ValueError:
+            print('\nInvalid input, please try again')
+
+
+# prompt the user for the width and the height of the box
+print('\nInput the width of your box')
+user_width = is_it_a_integer()
+
+
+# prompt the user for the height of the box
+print('\nInput the height of your box')
+user_height = is_it_a_integer()
+
+# create our box based off the user inputs
+# first create the top of the box
+print('*' * user_width)
+
+# while loop for the sides of the box
+i = 0
+while i < (user_height - 2):
+    print('*' + ' ' * (user_width - 2) + '*')
+    i = i + 1
+
+
+# then create the bottom of the box
+print('*' * user_width)
