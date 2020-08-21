b0f2b29b5d76c69c16ad7981aa438cedd05d710a
Pete Looney <petelooney81@gmail.com>
2020-08-21 15:59:14 -0500

    added some comments to groceries_list.py

--- groceries_list.py
+++ groceries_list.py
@@ -19,7 +19,7 @@ def print_list2():
         print(f'Item {counter2} is {groceries}')
         counter2 += 1
 
-
+# main menu function that calls our other functions
 def main_menu():
     while True:
         try:
@@ -42,11 +42,13 @@ def add_function():
     counter = 0
     # while loop
     while True:
+        # if it isn't the first time to run through this loop 
+        # (aka if counter doesn't equal zero, then we should ask if the user wants to delete another item)
         if counter != 0:
             user_input = input("Would you like to add another item?\n> ").lower()
         else:
             user_input = "yes"
-
+        # if the want to add something, the we append
         if user_input == "yes":
             item = input("What's the item? ")
             list.append(item)
@@ -57,11 +59,15 @@ def add_function():
         
         counter += 1
 
+# function for removing
 def remove_function():
     counter2 = 0
     # ask if the user want to remove an item
     while True:
+        # print the list without the main menu call (aka printlist2())
         print_list2()
+        # if it the first time to loop through this, then we don't need to ask if we want to remove another
+        # if it isn't the first time, then we ask if they want to remove another
         if counter2 != 0:
             wants_to_remove = input("Do you want to remove another item?\n> ").lower()
         else:
@@ -86,4 +92,6 @@ def remove_function():
             print('There is nothing left in here')
             main_menu()
         counter2 += 1
+
+# call the main menu function in order to start the game
 main_menu()
