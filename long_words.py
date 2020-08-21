7fdca00b2817aed853855513c0fe639dced837da
Pete Looney <petelooney81@gmail.com>
2020-08-21 10:43:39 -0500

    reworked long_words.py

--- long_words.py
+++ long_words.py
@@ -15,6 +15,9 @@ while True:
     except ValueError:
         break
 
+# before long variable
+before_long = user_input
+
 # convert it to lowercase for our if statement below (except for the first letter)
 user_input = user_input[0] + user_input[1:].lower()
 
@@ -26,12 +29,22 @@ length_of_string = len(user_input)
 
 # for loop to go through each letter in the word the user has inputted.
 for characters in user_input:
+    # if statement to see if counter != length_of_string -1
+    if counter != (length_of_string - 1):
+        # next character variable
+        next_char =  user_input[counter + 1]
+    else:
+        next_char = ''
     # initiate a boolean var called is a vowel, will be true if a character is a vowel
     is_vowel = (characters == 'a' or characters == 'e' or characters == 'i' or characters == 'o' or characters == 'u')
 
     # we don't want any of the leading characters or last characters to be changed even if it is a vowel
-    if is_vowel and (counter != 0 or counter != (length_of_string - 1)):
-        user_input = user_input.replace(characters, characters * 5, 1)
+    if is_vowel and (characters == next_char):
+        user_input = user_input.replace(characters+characters, characters * 5, 1)
+        break
+    
+    # iterate the counter
+    counter += 1
 
 # print the new word with replaced vowels
-print(user_input)
+print(f'\n\nYou entered {before_long}:\nThis word expanded is => {user_input}\n')
