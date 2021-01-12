135393125571853756fe01e0a91491b36f33564b
Pete Looney <petelooney81@gmail.com>
2021-01-12 16:10:48 -0600

    more redo

---
+++ redo/hello.py
@@ -0,0 +1,5 @@
+# name = input('What is your name? '.upper())
+# print(f'Hello, {name}!\nYour name has {len(name)} letters in in! Awesome!'.upper())
+
+name = input('What is your name? ')
+print('Hello, ' + name + '!\nYour name has ' + str(len(name)) + ' letters in it! Awesome')
--- redo/practice.py
+++ redo/practice.py
@@ -4,3 +4,5 @@ print(f'What is this "{food}" you speak of')
 print(f"What is this '{food}' you speak of")
 print(f'''What is this '{food}' you speak of''')
 print(len(f'''What is this `{food}` you speak of'''))
+
+print("whats up %s, you look %s today" %('10', 'wonderful'))
