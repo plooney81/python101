32ed4a2a51d3b5ad8a2d5e8d257632c374d5160f
Pete Looney <petelooney81@gmail.com>
2021-01-12 12:04:59 -0600

    redo

---
+++ redo/practice.py
@@ -0,0 +1,6 @@
+print("What is this \"pancakes\" you speak of")
+food = 'pancakes'
+print(f'What is this "{food}" you speak of')
+print(f"What is this '{food}' you speak of")
+print(f'''What is this '{food}' you speak of''')
+print(len(f'''What is this `{food}` you speak of'''))
